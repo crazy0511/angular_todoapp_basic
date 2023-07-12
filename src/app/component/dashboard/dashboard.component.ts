326c96f9f5753f897a7bd2048061533548f92c19
crazy0511 <phamquangtua8@gmail.com>
2023-07-12 15:00:22 +0700

    chú thích phần component dashboard

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -5,8 +5,11 @@ import { CrudService } from 'src/app/service/crud.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
 })
+
+// OnInit là 1 interface 
+// Một lớp (class) có thể implement (thực thi) một hoặc nhiều interface bằng cách 
+// cung cấp các thân (implementation) cho các phương thức và thuộc tính được định nghĩa trong interface đó.
 export class DashboardComponent implements OnInit{
 
   taskObj: Task = new Task();
@@ -16,17 +19,26 @@ export class DashboardComponent implements OnInit{
   editTaskValue: string = '';
 
   constructor(private crudService: CrudService){
-
   }
 
   ngOnInit(): void{
-    this.editTaskValue = '';
-    this.addTaskValue = '';
-    this.taskObj = new Task();
-    this.taskArr = [];
-    this.getAllTask();
+    // Khởi tạo các biến và lấy dữ liệu từ API
+    this.editTaskValue = '';  //string
+    this.addTaskValue = '';   //string
+    this.taskObj = new Task();//Task
+    this.taskArr = [];        //array: lưu trữ array task trong database
+    this.getAllTask();      
+    // console.log('editTaskValue', this.editTaskValue)
+    // console.log('addTaskValue', this.addTaskValue)
+    // console.log(this.taskObj)
+    // console.log(this.taskArr)
+    // console.log(this.getAllTask())
+
   }
 
+  // 1. Gọi API getAllTask() từ CrudService để lấy danh sách các task.
+  // 2. Nếu thành công, sẽ lưu kết quả trả về (đó là một mảng các task) vào biến taskArr.
+  // 3. Nếu thất bại (err), sẽ hiện thông báo "Unable to get list of tasks" cho người dùng.
   getAllTask(){
     this.crudService.getAllTask().subscribe(res => {
       this.taskArr = res;
@@ -36,24 +48,45 @@ export class DashboardComponent implements OnInit{
   }
 
   addTask(){
-    this.taskObj.task_name = this.addTaskValue;
-    this.crudService.addTask(this.taskObj).subscribe(res => {
-      this.ngOnInit();
-      this.addTaskValue = '';
-    }, err => {
-      alert(err);
-    })
+    // Nếu addTaskValue không rỗng thì thực hiện lệnh trong if
+    if(this.addTaskValue != ''){
+      // Gán task_name = addTaskValue
+      this.taskObj.task_name = this.addTaskValue;
+      // Thực hiện lệnh addTask trong CrudService
+      // this.taskObj là đối tượng task cần thêm, chứa các thông tin của task.
+      // 1. Gọi hàm addTask() từ CrudService để thêm task mới.
+      // 2. Nếu thêm thành công, Gọi hàm ngOnInit() để cập nhật lại danh sách task mới. Reset biến addTaskValue trở về rỗng.
+      this.crudService.addTask(this.taskObj).subscribe(res => {
+        // console.log('editTaskValue', this.editTaskValue)
+        // console.log('addTaskValue', this.addTaskValue)
+        // console.log(this.taskObj)
+        // console.log(this.taskArr)
+        // console.log(this.getAllTask())
+        this.ngOnInit();
+        this.addTaskValue = '';
+      }, err => {
+        alert(err);
+      })
+    }
   }
+
 
   editTask(){
-    this.taskObj.task_name = this.editTaskValue;
-    this.crudService.editTask(this.taskObj).subscribe(res => {
-      this.ngOnInit();
-    }, err => {
-      alert("Failed to update task");
+    if(this.editTaskValue != ''){
+      this.taskObj.task_name = this.editTaskValue;
+      this.crudService.editTask(this.taskObj).subscribe(res => {
+        this.ngOnInit();
+      }, err => {
+        alert("Failed to update task");
     })
+    }
   }
 
+
+  // 1. Nó nhận vào một đối tượng Task là task cần xóa, được truyền vào hàm dưới dạng đối số etask.
+  // 2. Gọi API deleteTask() từ CrudService để xóa task đó.
+  // 3. Nếu xóa thành công, gọi lại hàm ngOnInit() để cập nhật lại danh sách task mới sau khi xóa.
+  // 4. Nếu xóa thất bại, hiện thông báo lỗi cho người dùng.
   deleteTask(etask: Task){
     this.crudService.deleteTask(etask).subscribe(res => {
       this.ngOnInit();
@@ -63,7 +96,9 @@ export class DashboardComponent implements OnInit{
   }
 
   call(etask: Task){
+    // taskOjb = etask
     this.taskObj = etask;
+    // editTaskValue = task_name
     this.editTaskValue = etask.task_name;
   }
 }
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Task } from '../model/task';
 import { Observable } from 'rxjs';
 
-
+// Service dùng chung không phải khai báo vào module
 @Injectable({
   providedIn: 'root'
 })
@@ -11,14 +11,22 @@ export class CrudService {
 
   serviceURL: string;
 
+  // Khởi tạo đối tường http
   constructor(private http: HttpClient) {
     this.serviceURL = "https://64abae319edb4181202e4ec9.mockapi.io/api/todoapp";
   }
 
+  // Thực hiện việc gửi yêu cầu HTTP POST đến một API endpoint để thêm một task mới vào hệ thống. 
+  // task là đối tượng truyền vào
+  // Observable<Task>: là một Observable của kiểu dữ liệu Task, đại diện cho kết quả trả về từ yêu cầu HTTP POST.
+  // this.http.post() được sử dụng để gửi yêu cầu HTTP POST đến API endpoint được cung cấp bởi this.serviceURL.
+  // Tham số đầu tiên của phương thức post() là đường dẫn của API endpoint
+  // Tham số thứ hai là dữ liệu cần gửi đến API endpoint --> task
   addTask(task: Task): Observable<Task> {
     return this.http.post<Task>(this.serviceURL, task);
   }
 
+  // Kết quả trả về là Array Task lấy từ Database
   getAllTask(): Observable<Task[]> {
     return this.http.get<Task[]>(this.serviceURL);
   }
